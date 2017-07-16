0b20b14838f59c4498762c900861e90ebf6b26ef
sunil pai gurpur <sunilpaig@users.noreply.github.com>
2017-07-16 22:56:34 +0530

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -5,6 +5,5 @@ plot3 <- function() {
         legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
         dev.copy(png, file="plot3.png", width=480, height=480)
         dev.off()
-        cat("plot3.png has been saved in", getwd())
 }
 plot3()
